d28175b0fdc5c75bf1c553d986860f2f81302760
Arthur Torres <arthurtorres002@gmail.com>
2018-11-18 16:07:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combination|
-    win_combination[0] == "X" && win_combination[1] == "X" && win_combination[2]
+    board[win_combination[0]] == "X" && win_combination[1] == "X" && win_combination[2] == "X"
   end
 end
 
