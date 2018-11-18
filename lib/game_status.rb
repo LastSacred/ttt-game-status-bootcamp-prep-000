76eee33839768d186e9d09e24e5c8b1e0c506487
Arthur Torres <arthurtorres002@gmail.com>
2018-11-18 15:50:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,22 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def win?(board)
+  WIN_COMBINATIONS.any? |win_combination| do
+    win_combination.all? |win_index| do
+      board(win_index) == "X"
+    end
+  end
+end
+
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
